7ab77589dc241508676b67f11d2715b1ca0e0f4c
Francis Fernandez <francisfernandez0679@gmail.com>
2023-11-25 16:49:03 +0800

    Setup vue router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/views/Home.vue')
+    }
+  ]
+})
+
+createApp(App).use(router).mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col gap-4 items-center justify-center h-screen text-gray-100 bg-gray-800">
+    <h1 className="text-5xl font-bold text-center">Vue 3 Starter Kit</h1>
+    <p class="text-gray-400 font-thin text-lg italic text-center">
+      Made by:
+      <a
+        class="text-blue-400 transition duration-300 hover:underline hover:decoration-wavy"
+        href="https://github.com/Ax1a"
+        target="_blank"
+        >Axia</a
+      >
+    </p>
+  </div>
+</template>
+<script setup></script>
+<style lang=""></style>
